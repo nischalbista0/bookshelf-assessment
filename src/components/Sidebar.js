c22cf8894e5dfc0a0e0c32a7e8bfdbfffaed14ff
Nischal Bista <84696003+nischalbista0@users.noreply.github.com>
2023-09-13 00:22:16 +0545

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,11 +4,7 @@ import { FiBook, FiHome, FiSearch } from "react-icons/fi";
 const Sidebar = ({ activeTab, handleTabClick }) => {
   return (
     <div className="fixed z-20 bg-white dark:bg-black bottom-0 w-full md:bg-black md:w-[280px] md:min-h-[100vh] md:flex md:flex-col md:justify-between md:py-0">
-      <div className="md:flex md:flex-col md:gap-10">
-        <div className="hidden md:block md:px-8">
-          {/* <img src={BrandLogo} alt="" className="w-44" /> */}
-        </div>
-
+      <div className="md:mt-10 md:flex md:flex-col md:gap-10">
         <nav className="w-full md:px-4">
           <p className="hidden text-white opacity-75 mb-2 md:block md:ml-4">
             Navigation
@@ -19,7 +15,7 @@ const Sidebar = ({ activeTab, handleTabClick }) => {
               className={`relative hover:text-purple-lighter hover:dark:text-purple-lighter cursor-pointer transition duration-200 ease-linear md:flex md:items-center md:gap-3 md:w-full md:px-4 md:py-2.5 md:rounded-md md:hover:bg-dark-bg ${
                 activeTab === "home"
                   ? "text-purple-lighter dark:text-purple-lighter md:text-purple-lighter md:bg-dark-bg"
-                  : "text-black md:text-white"
+                  : "text-black md:text-white dark:text-white"
               }`}
               onClick={() => handleTabClick("home")}
             >
@@ -35,7 +31,7 @@ const Sidebar = ({ activeTab, handleTabClick }) => {
               className={`relative hover:text-purple-lighter hover:dark:text-purple-lighter cursor-pointer transition duration-200 ease-linear md:flex md:items-center md:gap-3 md:w-full md:px-4 md:py-2.5 md:rounded-md md:hover:bg-dark-bg ${
                 activeTab === "search"
                   ? "text-purple-lighter dark:text-purple-lighter md:text-purple-lighter md:bg-dark-bg"
-                  : "text-black md:text-white"
+                  : "text-black md:text-white dark:text-white"
               }`}
               onClick={() => handleTabClick("search")}
             >
@@ -51,7 +47,7 @@ const Sidebar = ({ activeTab, handleTabClick }) => {
               className={`relative hover:text-purple-lighter hover:dark:text-purple-lighter cursor-pointer transition duration-200 ease-linear md:flex md:items-center md:gap-3 md:w-full md:px-4 md:py-2.5 md:rounded-md md:hover:bg-dark-bg ${
                 activeTab === "bookshelf"
                   ? "text-purple-lighter dark:text-purple-lighter md:text-purple-lighter md:bg-dark-bg"
-                  : "text-black md:text-white"
+                  : "text-black md:text-white dark:text-white"
               }`}
               onClick={() => handleTabClick("bookshelf")}
             >
